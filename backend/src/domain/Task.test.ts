241951d88af1dda0c183805cd55bde9ee2727424
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2024-09-10 17:56:34 -0300

    test: changes the title

--- backend/src/domain/Task.test.ts
+++ backend/src/domain/Task.test.ts
@@ -3,6 +3,12 @@ import Task from './Task';
 describe('Task', () => {
   it('should be able to create a task', () => {
     const sut = new Task('any_task');
-    expect(sut.getTitle()).toBe('any_task');
+    expect(sut.getTitle()).toEqual('any_task');
+  });
+
+  it('should changes the title', () => {
+    const sut = new Task('any_task');
+    sut.changeTitle('new_title');
+    expect(sut.getTitle()).toEqual('new_title');
   });
 });
