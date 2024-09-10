6d6bfd5c276bd8759f6f0053e5d86514f29c22d1
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2024-09-10 19:53:52 -0300

    test: add a task in list

--- backend/src/domain/TasksList.test.ts
+++ backend/src/domain/TasksList.test.ts
@@ -1,4 +1,5 @@
 import TasksList from './TasksList';
+import Task from './Task';
 
 describe('List Tasks', () => {
   it('should change the title of the list', () => {
@@ -12,5 +13,13 @@ describe('List Tasks', () => {
     sut.changeTitle('');
     expect(sut.getTitle()).not.toEqual('');
     expect(sut.getTitle()).toEqual('New Tasks List');
+  });
+
+  it('should add a task to the list', () => {
+    const sut: TasksList = new TasksList('any_list');
+    const task: Task = new Task('any_task');
+    sut.addTask(task);
+    expect(sut.getList().length).toBe(1);
+    expect(sut.getList()[0]).toEqual(task);
   });
 });
