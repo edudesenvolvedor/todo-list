c187a9cd4d1b6370c2a5e1d060655b28c48d0e1b
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2024-09-10 17:35:24 -0300

    test: the title should not be empty

--- backend/src/domain/TasksList.test.ts
+++ backend/src/domain/TasksList.test.ts
@@ -6,4 +6,11 @@ describe('List Tasks', () => {
     sut.alterTitle('new_title');
     expect(sut.getTitle()).toEqual('new_title');
   });
+
+  it('the title should not be empty', () => {
+    const sut = new TasksList('any_list');
+    sut.alterTitle('');
+    expect(sut.getTitle()).not.toEqual('');
+    expect(sut.getTitle()).toEqual('New Tasks List');
+  });
 });
