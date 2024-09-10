779499a15fb28bf03cd5dd85b65c435a082a18fa
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2024-09-10 19:55:40 -0300

    refactor: define types in variables

--- backend/src/domain/Task.test.ts
+++ backend/src/domain/Task.test.ts
@@ -2,18 +2,18 @@ import Task from './Task';
 
 describe('Task', () => {
   it('should be able to create a task', () => {
-    const sut = new Task('any_task');
+    const sut: Task = new Task('any_task');
     expect(sut.getTitle()).toEqual('any_task');
   });
 
   it('should changes the title', () => {
-    const sut = new Task('any_task');
+    const sut: Task = new Task('any_task');
     sut.changeTitle('new_title');
     expect(sut.getTitle()).toEqual('new_title');
   });
 
   it('the title should not be empty', () => {
-    const sut = new Task('any_task');
+    const sut: Task = new Task('any_task');
     sut.changeTitle('');
     expect(sut.getTitle()).not.toEqual('');
     expect(sut.getTitle()).toEqual('New Task');
@@ -21,7 +21,7 @@ describe('Task', () => {
 
   it('should not be longer than 50 characters', () => {
     expect(() => {
-      const sut = new Task('a'.repeat(60));
+      const sut: Task = new Task('a'.repeat(60));
     }).toThrow('Title with more than 50 characters');
   });
 });
--- backend/src/domain/TasksList.test.ts
+++ backend/src/domain/TasksList.test.ts
@@ -3,13 +3,13 @@ import Task from './Task';
 
 describe('List Tasks', () => {
   it('should change the title of the list', () => {
-    const sut = new TasksList('any_list');
+    const sut: TasksList = new TasksList('any_list');
     sut.changeTitle('new_title');
     expect(sut.getTitle()).toEqual('new_title');
   });
 
   it('the title should not be empty', () => {
-    const sut = new TasksList('any_list');
+    const sut: TasksList = new TasksList('any_list');
     sut.changeTitle('');
     expect(sut.getTitle()).not.toEqual('');
     expect(sut.getTitle()).toEqual('New Tasks List');
