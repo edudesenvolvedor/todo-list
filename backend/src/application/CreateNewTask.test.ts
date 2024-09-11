3dc751e599be83d5a5dcb967faf3795b597770bf
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2024-09-10 21:42:30 -0300

    test: save the task in repository in memory

--- backend/src/application/CreateNewTask.test.ts
+++ backend/src/application/CreateNewTask.test.ts
@@ -1,14 +1,22 @@
-import TasksList from '../domain/TasksList';
 import Task from '../domain/Task';
 import CreateNewTask from './CreateNewTask';
+import TaskRepositoryInMemory from '../repository/TaskRepositoryInMemory';
 
 describe('Application: CreateNewTask', () => {
   it('should create a task', () => {
-    const tasksList: TasksList = new TasksList('any_tasks');
-    const sut: CreateNewTask = new CreateNewTask(tasksList);
+    const sut: CreateNewTask = new CreateNewTask(new TaskRepositoryInMemory());
     const task: Task = new Task('any_task');
     const newTask: Task = sut.execute(task);
 
     expect(newTask).toEqual(task);
+  });
+
+  it('should be possible to save the task', () => {
+    const repository: TaskRepositoryInMemory = new TaskRepositoryInMemory();
+    const sut: CreateNewTask = new CreateNewTask(repository);
+    const task: Task = new Task('any_task');
+    sut.execute(task);
+
+    expect(repository.getAll()[0]).toEqual(task);
   });
 });
